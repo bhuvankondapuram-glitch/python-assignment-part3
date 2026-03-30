4c2821ae527e62bc9a7560554afc597a4c6b7c9b
bhuvankondapuram-glitch <bhuvan.kondapuram@gmail.com>
2026-03-30 21:08:54 +0530

    Update part3_api_files.py

--- part3_api_files.py
+++ part3_api_files.py
@@ -1,11 +1,1 @@
-try:
-    # Create and write notes
-    with open("python_notes.txt", "w") as f:
-        f.write("Python is easy to learn.\nFile handling is useful.")
 
-    # Force an error (to create error_log.txt)
-    print(10 / 0)
-
-except Exception as e:
-    with open("error_log.txt", "w") as err:
-        err.write(str(e))
